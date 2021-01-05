853df87382b3766e261ee0d0200f915bbffad71d
dave-cmdr <david.cassie.mail@gmail.com>
2021-01-05 21:58:56 +0000

    firstcommit

---
+++ src/test/java/cmdr/dave/mealplanner/MealplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package cmdr.dave.mealplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
