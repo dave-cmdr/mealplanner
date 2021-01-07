5b096f5443c8f60cc3258d29509ea40fa6a7ae6a
dave-cmdr <david.cassie.mail@gmail.com>
2021-01-07 20:08:53 +0000

    add recipe map service

---
+++ mealplanner-data/src/main/java/cmdr/dave/mealplanner/services/map/RecipeMapService.java
@@ -0,0 +1,36 @@
+package cmdr.dave.mealplanner.services.map;
+
+import cmdr.dave.mealplanner.model.Recipe;
+import cmdr.dave.mealplanner.services.RecipeService;
+
+import java.util.Set;
+
+/**
+ * @author dcass
+ */
+public class RecipeMapService extends AbstractMapService<Recipe, Long> implements RecipeService {
+    @Override
+    public Set<Recipe> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Recipe object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Recipe save(Recipe object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+        return super.findById(id);
+    }
+}
